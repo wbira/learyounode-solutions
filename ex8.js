565ce8385ea8f5552104f080a61ac31b24670595
Waldemar Bira <biraw@ryanair.com>
2018-07-10 11:45:34 +0200

    Implemented solution for exercise 8 without external lib

--- ex8.js
+++ ex8.js
@@ -3,6 +3,7 @@ const bl = require('bl');
 
 const url = process.argv[2];
 
+// solution with external lib
 http.get(url, response => {
   response.pipe(
     bl((err, data) => {
@@ -14,4 +15,18 @@ http.get(url, response => {
       console.log(data);
     })
   );
+});
+
+// solution without external lib
+let results = [];
+http.get(url, response => {
+  response.setEncoding('utf8');
+  response.on('data', data => {
+    results.push(data);
+  });
+  response.on('end', () => {
+    const data = results.join('');
+    console.log(data.length);
+    console.log(data);
+  });
 });
